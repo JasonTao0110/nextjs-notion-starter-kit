32d312cf7863e5dcec23ea44ade04a454d55d6f8
wt <tao_wang0110@163.com>
2023-03-04 17:32:20 +0800

    change for me

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '69300519aa2a465aa70f4f12edd1e3e0',
+  rootNotionPageId: 'jasonlearn/00c1ba3f0c00471aa93c3fa0b3521178?v=8064a971d243471e89b294a04c89f533',  // 后面跟在notion内共享页面复制到的link地址的后尾
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: 'o-id',
 
   // basic site info (required)
-  name: 'o-id',
-  domain: 'o-id.cc',
-  author: 'o-id',
+  name: 'Jason`s Blog',
+  // domain 后面填写你共享页面的Link  notion页面的标题切记不要更改，更改后你这个页面的link地址会有变化，这个代码里的修改就没用了
+  domain: 'https://www.notion.so/jasonlearn/00c1ba3f0c00471aa93c3fa0b3521178?v=8064a971d243471e89b294a04c89f533',
+  author: 'Jason',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'chusight1',
-  github: 'chusight',
-  zhihu: 'chusight',
+  twitter: 'jason',
+  github: 'jason',
+  zhihu: 'jason',
   // linkedin: '',
   // instagram: 'onet2lee',
 
